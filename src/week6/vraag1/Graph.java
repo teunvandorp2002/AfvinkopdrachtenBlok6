456bdb82bca0a8864eaa33e4a8b77c32a71816f7
teunvandorp2002 <teunvandorp2002@users.noreply.github.com>
2021-01-13 15:45:55 +0100

    Finish week 6

--- src/week6/vraag1/Graph.java
+++ src/week6/vraag1/Graph.java
@@ -3,9 +3,9 @@ package week6.vraag1;
 import java.util.*;
 
 class Graph {
-    public static void makeGraph(ArrayList<ArrayList<String>> genen) {
-        ArrayList<String> header = new ArrayList<>(genen.get(0));
-        ArrayList<String> seq = new ArrayList<>(genen.get(1));
+    public static void makeGraph(ArrayList<ArrayList<String>> genes) {
+        ArrayList<String> header = new ArrayList<>(genes.get(0));
+        ArrayList<String> seq = new ArrayList<>(genes.get(1));
         calc(seq, header);
     }
 
@@ -18,15 +18,15 @@ class Graph {
                 String y = seq.get(j);
                 for (int k = 0; k < x.length(); k++) {
                     if (x.length() / 2 < x.length() - k) {
-                        char posI = x.charAt(x.length() / 2 + k);
-                        char posJ = y.charAt(k);
-                        if (posI == posJ) {
+                        char pos1 = x.charAt(x.length() / 2 + k);
+                        char pos2 = y.charAt(k);
+                        if (pos1 == pos2) {
                             count++;
                         }
                     }
                 }
                 if (!header.get(i).equals(header.get(j)) && count > 1) {
-                    out += (header.get(i) + " overlapt met " + header.get(j) + "\n");
+                    out += (header.get(i) + " overlaps with " + header.get(j) + "\n");
                 }
                 count = 0;
             }
